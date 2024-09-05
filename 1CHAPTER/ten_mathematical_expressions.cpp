6376e927fdef76cf7e13e3d11afef3582714a6eb
luisolavarria <elproluiss@gmail.com>
2024-09-05 14:16:32 -0400

    First expression

---
+++ 1CHAPTER/ten_mathematical_expressions.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+
+
+int main(){
+    int x=5;
+    int y=3;
+    int z=8;
+    int a=2;
+    cout << "the product of "<< x << " and " << y << " is ";
+    cout << x * y << "." << endl; 
+    return 0;
+    }   
