d059aa56a1e20f8a89f1760664bdf71b159ba73d
luisolavarria <elproluiss@gmail.com>
2024-09-13 00:52:28 -0400

    Set0 updates

---
+++ 3CHAPTER/Set0/Exercise3/Question1.cpp
@@ -0,0 +1,11 @@
+
+//First Question
+#include <iostream>
+using namespace std;
+int square(int x) {
+    return x * x ;}
+    int main() {     
+   int y = 10;
+    square(y);   //The result of the Function is discarted because is not being used in a expression.
+    return 0;
+}
---
+++ practice.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+double sales = 95000;
+cout<< "sales = "<< sales<<endl;
+double StateTax = (4);
+cout<< "State Tax = "<< StateTax<<"%"<<endl;
+double CountyTax = (2);
+cout<< "County Tax = "<< CountyTax<<"%"<<endl;
+
+return 0;
+}
