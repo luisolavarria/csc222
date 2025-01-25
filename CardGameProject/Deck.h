1b3349a92ab1976f689d64f9b5b63c2a70ba9d5c
Luis Alejandro Garcés Olavarría <elproluisss@gmail.com>
2025-01-25 13:02:11 -0500

    Deck.h

--- CardGameProject/Deck.h
+++ CardGameProject/Deck.h
@@ -1,24 +1,23 @@
-#include <iostream>
+#ifndef DECK_H
+#define DECK_H
+
+#include "Card.h"
+#include <vector>
 
 class Deck {
- std::vector<Card> cards; 
- int currentCardIndex; 
- public: Deck() { 
-  for (int suit = 0; suit < 4; ++suit) {
-     for (int rank = 2; rank <= 14; ++rank) {
-         cards.push_back(Card(rank, suit));
-          } 
-          }
- currentCardIndex = 0;
-  } void shuffle() {
- std::random_shuffle(cards.begin(), cards.end());}
-  Card deal() { 
-   if (currentCardIndex < cards.size()) { 
-    return cards[currentCardIndex++]; }
-    else {
-throw std::out_of_range("No cards left in the deck!");}
- }
-  bool isEmpty() const {
- return currentCardIndex >= cards.size();} 
+private:
+    std::vector<Card> cards;
+    int currentCardIndex;    
+
+public:
+    
+    Deck();
+
+   
+    void shuffle();               
+    Card deal();                 
+    bool isEmpty() const;         
+    void displayDeck() const;   
 };
-h
+
+#endif
